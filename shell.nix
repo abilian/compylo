ce5a64ab01a7b3366e9f7a2992a3098da99a4810
Ethan Zouzoulkowsky <ethan.zouzoulkowsky@gmail.com>
2022-09-19 14:17:50 +0200

    feat(nix): added basic shell.nix

---
+++ shell.nix
@@ -0,0 +1,10 @@
+{ pkgs ? import <nixpkgs> {}} :
+
+pkgs.mkShell {
+  buildInputs = with pkgs; [
+    python310
+    wasmer
+    llvm  # FIXME: this is LLVM v11.0, is it the one we want or should we use
+          # a more recent version ?
+  ];
+}
