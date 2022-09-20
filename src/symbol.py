7c4f516ecc164511b083ba769602b929342f004e
Ethan Zouzoulkowsky <ethan.zouzoulkowsky@gmail.com>
2022-09-20 18:00:11 +0200

    feat(symbol): added Symbol class, which will be used in type processing to store informations about object, their names, types, etc

---
+++ src/symbol.py
@@ -0,0 +1,17 @@
+class Symbol:
+    counter = 0
+
+    def __init__(self, name=None):
+        self.type = None
+        if (name is None):
+            self.name = 'tmp'
+        else:
+            self.name = name
+        self.name += f'__{self.counter}'
+        Symbol.counter += 1
+
+    def __str__(self):
+        return self.name
+
+    def __eq__(self, other):
+        return self.type == other and self.name == other.name
