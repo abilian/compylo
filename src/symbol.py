b6751ad06446a49d3bf7a8d4ed086e32b6758daa
Ethan Zouzoulkowsky <ethan.zouzoulkowsky@gmail.com>
2022-09-21 12:38:49 +0200

    fix(symbol): fixed the rename system, as renaming a user-defined symbol without renaming it everywhere in the ast makes no sense

--- src/symbol.py
+++ src/symbol.py
@@ -4,11 +4,10 @@ class Symbol:
     def __init__(self, name=None):
         self.type = None
         if (name is None):
-            self.name = 'tmp'
+            self.name = 'tmp__{self.counter}'
+            Symbol.counter += 1
         else:
             self.name = name
-        self.name += f'__{self.counter}'
-        Symbol.counter += 1
 
     def __str__(self):
         return f'{self.name} : {self.type}'
