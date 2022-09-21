ddd27dc8dc471096905d2544cfff87c746e414b3
Ethan Zouzoulkowsky <ethan.zouzoulkowsky@gmail.com>
2022-09-21 12:44:57 +0200

    feat(typeVisitor): success ! For basic program and annotated function

--- src/typeVisitor.py
+++ src/typeVisitor.py
@@ -3,6 +3,8 @@ from symbol import *
 import ast
 
 class TypeVisitor():
+    ifCounter = 0
+
     def __init__(self, scopedMap):
         self.scopedMap = scopedMap
 
@@ -13,106 +15,144 @@ class TypeVisitor():
             'bool': bool
         }
 
+    def _visitWithPredicate(self, node, predicate):
+        # TODO: Useful, can be used to find a return statement embedded
+        # somewhere in a function, or smth like that
+        pass
+
     def visit(self, node):
-        name = 'visit' + type(node).__name__
+        t = type(node).__name__
+        if (t[0].islower()): # prettier code
+            t = t[0].upper() + t[1:]
+
+        name = 'visit' + t
         fct = getattr(self, name)
 
         return fct(node)
 
     def visitModule(self, node):
-        print("visiting Module")
+        print('visiting Module')
         for t in node.body:
             self.visit(t)
 
 
     def visitExpr(self, node):
-        print("visiting Expr")
-        self.visit(node.value)
+        print('visiting Expr')
+        return self.visit(node.value)
 
 
     def visitBinOp(self, node):
-        print("visiting BinOp")
+        '''
+            FIXME: Pb: In Python, we can multiply string or float by int... How to
+            check for that ? In general, how to check for overloaded binary
+            operators ?
+        '''
+        print('visiting BinOp')
         typeLeft = self.visit(node.left)
         typeRight = self.visit(node.right)
 
         if typeLeft != typeRight:
-            raise TypeError(f'Cannot use operator {node.op} with terms of\
-                            different types') # FIXME: need a way to say where we are in the program
+            raise TypeError(f'Cannot use operator {node.op} with terms {typeLeft.__name__} and {typeRight.__name__}')
+        # FIXME: need a way to say where we are in the program
 
         return typeRight
 
 
     def visitConstant(self, node):
-        print("visiting Constant")
-        return type(node.value)
+        print('visiting Constant')
+        return type(node.value).__name__
 
 
     def visitAssign(self, node):
-        """
+        '''
             For now, assuming we're just doing assignments like `x = 1`
-        """
-        print("visiting Assign")
+        '''
+        print('visiting Assign')
         for t in node.targets:
             s = Symbol(t.id)
+            self.scopedMap.append(s) # Should it be append or update ?
             s.type = self.visit(node.value)
-
-        pass
-
-
-    def visitEq(self, node):
-        print("visiting Eq")
-        pass
 
 
     def visitCall(self, node):
-        print("visiting Call")
-        pass
+        print('visiting Call')
+        self.visit(node.func)
+        for a in node.args:
+            self.visit(a)
+
+        sym = self.scopedMap.find(node.func.id) # FIXME: what is node.func is
+                                                # not an ast.Name ? Can it ?
+        if sym is None:
+            raise NotImplementedError(f'No symbol to call for {node.func.id}')
+
+        return sym.type
 
 
     def visitFunctionDef(self, node):
-        """
+        '''
             - Create a new symbol for the function
             - Check the body for a Return node
                 If so, s.type = return.type
                 Else s.type = Void
             visit the body
-        """
-        print("visiting FunctionDef")
-        pass
+        '''
+        # FIXME: For now, only handling functions with annotations
+        print('visiting FunctionDef')
+        s = Symbol(node.name)
+        s.type = node.returns.id
+        self.scopedMap.update(s)
+        self.scopedMap.push_scope(node.name)
 
+        self.visit(node.args)
+
+        for e in node.body:
+            self.visit(e)
+
+        self.scopedMap.pop_scope()
 
     def visitArguments(self, node):
-        print("visiting Arguments")
-        pass
+        print('visiting Arguments')
+        # FIXME: incomplete
+        for a in node.args:
+            self.visit(a)
 
 
     def visitArg(self, node):
-        print("visiting Arg")
-        pass
+        print('visiting Arg')
+        s = Symbol(node.arg)
+        if node.annotation:
+            s.type = node.annotation.id
 
 
     def visitReturn(self, node):
-        print("visiting Return")
-        pass
+        print('visiting Return')
+        return self.visit(node.value)
 
+
+    def visitIf(self, node):
+        print('visiting If')
+        self.visit(node.test)
+
+        self.scopedMap.push_scope(f'if__{TypeVisitor.ifCounter}')
+        TypeVisitor.ifCounter += 1
+
+        for e in node.body:
+            self.visit(e)
+
+        print(self.scopedMap)
+
+        self.scopedMap.pop_scope()
 
     def visitName(self, node):
         print("visiting Name")
         pass
 
 
-    def visitLoad(self, node):
-        print("visiting Load")
-        pass
-
-
-    def visitIf(self, node):
-        print("visiting If")
-        pass
-
-
-    def visitStore(self, node):
-        pass
+    def visitCompare(self, node):
+        print('visiting Compare')
+        self.visit(node.left)
+        for c in node.comparators:
+            self.visit(c)
 
 
 if __name__ == '__main__':
@@ -120,4 +160,6 @@ if __name__ == '__main__':
         content = f.read()
 
     root = ast.parse(content)
-    TypeVisitor(None).visitModule(root)
+    scopedMap = ScopedMap()
+    t = TypeVisitor(scopedMap)
+    t.visitModule(root)
