bf77418ad0dc76cbfab713ed059f476c13547b4b
Ethan Zouzoulkowsky <ethan.zouzoulkowsky@gmail.com>
2022-10-28 14:57:29 +0200

    feat(binder): added basic comments

--- src/binder.py
+++ src/binder.py
@@ -2,6 +2,7 @@ from scopedMap import ScopedMap
 from symbol import Symbol
 import ast
 
+
 class Binder(ast.NodeVisitor):
     """
     First visitor to be ran, can most likely be refactor-ed within the other
@@ -11,12 +12,20 @@ class Binder(ast.NodeVisitor):
     def __init__(self):
         self.map = ScopedMap()
 
+    def __call__(self, node):
+        self.visit(node)
+        return self.map
+
     def visit_FunctionDef(self, node):
-        # First, add the function symbol
+        """
+        Create a symbol for the function, adds it to the scoped map.
+        Pushes a new scope and visits the function within this new scope
+        """
+
         sym = Symbol(node.name, definition=node)
         self.map.append(sym)
+        node.definition = node
 
-        # Then, add a scope corresponding to the function
         self.map.push_scope(sym)
         self.visit(node.args)
         for instr in node.body:
@@ -24,26 +33,41 @@ class Binder(ast.NodeVisitor):
 
         self.map.pop_scope()
 
-    def visit_arguments(self, node):
-        for a in node.args:
-            self.visit(a)
-
     def visit_arg(self, node):
+        """
+        Creates a symbol for the arg, adds it to the map and sets the definition
+        of the node to itself
+        """
         sym = Symbol(node.arg, definition=node)
         self.map.append(sym)
+        node.definition = node
 
-    def visit_AnnAssign(self, node: ast.AnnAssign):
+    def visit_AnnAssign(self, node):
+        """
+        Do not visit the annotation (temporary)
+        """
         self.visit(node.target)
         self.visit(node.value)
 
     def visit_Name(self, node):
+        """
+        Visits a name.
+        If context is `ast.Load`, variable is used as rValue
+            -> look it up in the table and throw error if undefined
+        Else if context is `ast.Store`, variable is used a lValue
+            -> create a new symbol and sets the definition of the node.
+            FIXME: for AugAssign, this doesn't work
+        Else (context is `ast.Del`)
+           -> remove the symbol ? FIXME: Not Implemented
+        """
         if isinstance(node.ctx, ast.Load):
-            sym = self.map.find(node.id)
+            sym = self.map.find(node.id, False)
             if sym is None:
-                raise Exception(f'Undefined symbol: {node.id}')
+                raise Exception(f"Undefined symbol: {node.id}")
             node.definition = sym.definition
         elif isinstance(node.ctx, ast.Store):
             sym = Symbol(node.id, definition=node)
             self.map.append(sym)
+            node.definition = node
         else:
-            raise NotImplementedError('del instruction not yet implemented')
+            raise NotImplementedError("del instruction not yet implemented")
