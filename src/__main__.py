d78092d5c3e5dc0f0ca40c1b486c204bae46a7ce
Ethan Zouzoulkowsky <ethan.zouzoulkowsky@gmail.com>
2022-11-10 18:30:26 +0100

    feat(main): started argument parsing (WIP)

--- src/__main__.py
+++ src/__main__.py
@@ -1,27 +1,78 @@
 from typeVisitor import TypeVisitor
+from printer import Printer
 from binder import Binder
 from renamer import Renamer
 from errors import *
+import argparse
 import sys, ast, traceback
 
 
-def main():
-    if len(sys.argv) != 2:
-        print("USAGE: python ./compylo.py FILE")
-        return 1
+def setup_parser():
+    parser = argparse.ArgumentParser(
+        prog="compylo",
+        description="Python\
+                                     to WASM Compiler using LLVVM",
+    )
 
-    file = sys.argv[1]
-    content = ""
+    parser.add_argument("FILENAME", help="the file to compile")
+    parser.add_argument(
+        "-P", "--print", help="print the processed ast", action="store_true"
+    )
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument(
+        "-B", "--bind", help="bind the ast", action="store_true"
+    )
+    group.add_argument(
+        "-R", "--rename", help="rename the variables", action="store_true"
+    )
+    group.add_argument(
+        "-T", "--type", help="compute types", action="store_true"
+    )
+
+    return parser
+
+
+def get_action(action: str):
+    actionMap = {
+        "print": Printer(),
+        "bind": Binder(),
+        "rename": Renamer(),
+        "type": TypeVisitor(),
+    }
+
+    return actionMap[action]
+
+
+def dependencies(action: str, node: ast.AST):
+    dependenciesMap = {
+        "print": [],
+        "bind": [],
+        "rename": ["bind"],
+        "type": ["rename"],
+    }
+
+    if not dependenciesMap[action]:
+        return get_action(action)(node)
+
+    dependencies(dependenciesMap[action][0], node)
+    # FIXME: with this approach, only flags can only have 1 dep
+
+    return get_action(action)(node)
+
+
+def main():
+    parser = setup_parser()
+    args = parser.parse_args()
+    print(args)
+    file = args.FILENAME
+
     with open(file) as f:
         content = f.read()
 
     root = ast.parse(content)
 
     try:
-        Binder()(root)
-        Renamer()(root)
-        TypeVisitor()(root)
-        TypeVisitor()(root)
+        pass
     except UnknownSymbolError as e:
         traceback.print_exception(e)
         sys.exit(2)
