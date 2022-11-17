a3704911462b4f5dadf7722c69439a3116177228
Ethan Zouzoulkowsky <ethan.zouzoulkowsky@gmail.com>
2022-11-17 12:40:09 +0100

    feat(tests): moved run_tests to __main__.py

---
+++ tests/__main__.py
@@ -0,0 +1,22 @@
+from run_tests import *
+
+if __name__ == "__main__":
+    passed = 0
+    failed = 0
+    total = 0
+
+    for d in DIRECTORIES:
+        testList = getTestList(f"{d}.yaml")
+        total += len(testList)
+        successfulTests = runTests(testList)
+        passed += successfulTests
+        failed += len(testList) - successfulTests
+
+    passed_message = tc.colored(f"PASSED: {passed}", "green")
+    failed_message = tc.colored(f"FAILED: {failed}", "red")
+    total_message = tc.colored(f"TOTAL: {total}", "blue")
+
+    print()
+    print(passed_message)
+    print(failed_message)
+    print(total_message)
--- tests/run_tests.py
+++ tests/run_tests.py
@@ -52,25 +52,3 @@ def runTests(testList: List[YamlTestCase]):
         return False
 
     return sum(map(run, testList))
-
-
-if __name__ == "__main__":
-    passed = 0
-    failed = 0
-    total = 0
-
-    for d in DIRECTORIES:
-        testList = getTestList(f"{d}.yaml")
-        total += len(testList)
-        successfulTests = runTests(testList)
-        passed += successfulTests
-        failed += len(testList) - successfulTests
-
-    passed_message = tc.colored(f"PASSED: {passed}", "green")
-    failed_message = tc.colored(f"FAILED: {failed}", "red")
-    total_message = tc.colored(f"TOTAL: {total}", "blue")
-
-    print()
-    print(passed_message)
-    print(failed_message)
-    print(total_message)
